3a6157691946f6981b590c8771eb7dc92508f57f
Sanderqs <channie.qs@gail.com>
2021-02-03 20:14:55 +0100

    Initial commit

---
+++ voorbeeld.js
@@ -0,0 +1,24 @@
+initialState = () => {
+  return {
+    data: {
+      mana: {
+        black: 1,
+        blue: 2,
+        green: 3,
+        red: 4,
+        white: 5,
+      },
+      status: "check",
+    },
+  };
+};
+//  naar res
+const res = initialState();
+console.log(res);
+
+//deconstruct
+const {
+  data: { mana },
+  status,
+} = initialState();
+console.log(mana, status);
